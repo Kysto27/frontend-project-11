f224681ae0b4d824209ccbf4a68de155477f4667
Aleksandr R <kysto27@gmail.com>
2023-06-16 15:21:18 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,62 @@
 import './style.scss';
 import 'bootstrap';
+import { object, string } from 'yup';
+import onChange from 'on-change';
+import isEmpty from 'lodash/isEmpty.js';
 
-console.log('Hello World!');
+const validate = (form, fields, addedFeeds) => {
+  const schema = object({
+    url: string().notOneOf(addedFeeds).url().nullable(),
+  });
+  schema
+    .validate(fields, { abortEarly: false })
+    .then(() => {
+      console.log('[ok]');
+    })
+    .catch((error) => {
+      return error.inner;
+    });
+};
+
+const render = (form, initialState) => {
+  if (initialState.form.valid === false) {
+    console.log('Форма невалидна');
+  } else {
+    console.log('Форма валидна');
+  }
+};
+
+const app = () => {
+  const addedFeeds = [];
+
+  const form = document.querySelector('.rss-form');
+
+  const initialState = {
+    form: {
+      valid: true,
+      processState: 'filling',
+      processError: null,
+      errors: {},
+    },
+    fields: {
+      url: '',
+    },
+  };
+
+  const state = onChange(initialState, render(form, initialState));
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const value = formData.get('url');
+    console.log(value);
+    state.fields.url = value;
+    const errors = validate(state.form, state.fields, addedFeeds);
+    state.form.errors = errors;
+    state.form.valid = isEmpty(errors);
+    console.log(errors);
+    addedFeeds.push(value);
+  });
+};
+
+app();
