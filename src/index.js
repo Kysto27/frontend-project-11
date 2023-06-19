735792ca06629917a79041fbc9e7d0eff7eb831c
Aleksandr R <kysto27@gmail.com>
2023-06-19 17:09:03 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -25,60 +25,70 @@ const renderError = (errorMessage) => {
 };
 
 const render = (state, elements, path, currentValue, previousValue) => {
-  const { form, input } = elements;
-  if (state.form.valid === false) {
-    input.classList.add('is-invalid');
-    const p = document.createElement('p');
-    p.textContent = renderError(state.form.errorMessage);
-    p.classList.add(
-      'feedback',
-      'm-0',
-      'position-absolute',
-      'small',
-      'text-success',
-      'text-danger',
-    );
-    form.append(p);
+  const { input, feedback } = elements;
+  console.log(state.form.processState);
+  switch (state.form.processState) {
+    case 'filling': {
+      input.value = '';
+      input.focus();
+      break;
+    }
+    case 'filled': {
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      feedback.textContent = 'RSS успешно загружен';
+      input.value = '';
+      input.focus();
+      break;
+    }
+    case 'validateError': {
+      input.classList.add('is-invalid');
+      feedback.textContent = renderError(state.form.errorMessage);
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      break;
+    }
+    default:
+      throw new Error(`Unknown state: ${state.form.processState}!`);
   }
 };
 
 const app = () => {
-  const addedFeeds = [];
-
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
+    feedback: document.querySelector('.feedback'),
   };
 
-  const initialState = {
+  const state = {
     form: {
-      valid: true,
       processState: 'filling',
-      processError: null,
       errorMessage: null,
     },
+    content: {
+      feeds: [],
+    },
   };
 
-  const watchedState = onChange(
-    initialState,
-    (path, currentValue, previousValue) => {
-      render(watchedState, elements, path, currentValue, previousValue);
-    },
-  );
+  const watchedState = onChange(state, (path, currentValue, previousValue) => {
+    render(watchedState, elements, path, currentValue, previousValue);
+  });
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(elements.form);
     const value = formData.get('url');
-    validate(value, addedFeeds)
-      .then(() => console.log('ok'))
+    validate(value, state.content.feeds)
+      .then(() => {
+        watchedState.form.processState = 'filled';
+        watchedState.content.feeds.push(value);
+        watchedState.form.errorMessage = null;
+      })
       .catch((error) => {
         watchedState.form.errorMessage = error.message;
-        // this must not be one of the following values
-        // this must be a valid URL
-        watchedState.form.valid = false;
+        watchedState.form.processState = 'validateError';
       });
-    addedFeeds.push(value);
   });
 };
 
